9315b771b930e2a6fca1730e3621a711fb7b3c4a
qtx73 <qtx73nn@gmail.com>
2025-01-19 21:40:11 +0900

    Implement RISC-V instruction decoder

--- rv32emu.c
+++ rv32emu.c
@@ -5,6 +5,323 @@
 #include <stdbool.h>
 #include <assert.h>
 
+typedef enum {
+    INSTR_LUI,
+    INSTR_AUIPC,
+    INSTR_JAL,
+    INSTR_JALR,
+    INSTR_BEQ,
+    INSTR_BNE,
+    INSTR_BLT,
+    INSTR_BGE,
+    INSTR_BLTU,
+    INSTR_BGEU,
+    INSTR_LB,
+    INSTR_LH,
+    INSTR_LW,
+    INSTR_LBU,
+    INSTR_LHU,
+    INSTR_SB,
+    INSTR_SH,
+    INSTR_SW,
+    INSTR_ADDI,
+    INSTR_SLTI,
+    INSTR_SLTIU,
+    INSTR_XORI,
+    INSTR_ORI,
+    INSTR_ANDI,
+    INSTR_SLLI,
+    INSTR_SRLI,
+    INSTR_SRAI,
+    INSTR_ADD,
+    INSTR_SUB,
+    INSTR_SLL,
+    INSTR_SLT,
+    INSTR_SLTU,
+    INSTR_XOR,
+    INSTR_SRL,
+    INSTR_SRA,
+    INSTR_OR,
+    INSTR_AND,
+    INSTR_ECALL
+} Instruction;
+
+Instruction decoded_instr;
+
+void decoder(uint32_t instr) {
+    uint32_t opcode = instr & 0x7F;
+    uint32_t rd = (instr >> 7) & 0x1F;
+    uint32_t rs1 = (instr >> 15) & 0x1F;
+    uint32_t rs2 = (instr >> 20) & 0x1F;
+    uint32_t funct3 = (instr >> 12) & 0x7;
+    uint32_t funct7 = (instr >> 25) & 0x7F;
+
+    switch (opcode) {
+        case 0x37 : // LUI (U-type)
+            decoded_instr = INSTR_LUI;
+            break;
+        case 0x17 : // AUIPC (U-type)
+            decoded_instr = INSTR_AUIPC;
+            break;
+        case 0x6F : // JAL (J-type)
+            decoded_instr = INSTR_JAL;
+            break;
+        case 0x67 : // JALR (I-type)
+            decoded_instr = INSTR_JALR;
+            break;
+        case 0x63 : // Branch instructions
+            switch (funct3) {
+                case 0x0 : // BEQ
+                    decoded_instr = INSTR_BEQ;
+                    break;
+                case 0x1 : // BNE
+                    decoded_instr = INSTR_BNE;
+                    break;
+                case 0x4 : // BLT
+                    decoded_instr = INSTR_BLT;
+                    break;
+                case 0x5 : // BGE
+                    decoded_instr = INSTR_BGE;
+                    break;
+                case 0x6 : // BLTU
+                    decoded_instr = INSTR_BLTU;
+                    break;
+                case 0x7 : // BGEU
+                    decoded_instr = INSTR_BGEU;
+                    break;
+            }
+            break;
+        case 0x03 : // Load instructions
+            switch (funct3) {
+                case 0x0 : // LB
+                    decoded_instr = INSTR_LB;
+                    break;
+                case 0x1 : // LH
+                    decoded_instr = INSTR_LH;
+                    break;
+                case 0x2 : // LW
+                    decoded_instr = INSTR_LW;
+                    break;
+                case 0x4 : // LBU
+                    decoded_instr = INSTR_LBU;
+                    break;
+                case 0x5 : // LHU
+                    decoded_instr = INSTR_LHU;
+                    break;
+            }
+            break;
+        case 0x23 : // Store instructions
+            switch (funct3) {
+                case 0x0 : // SB
+                    decoded_instr = INSTR_SB;
+                    break;
+                case 0x1 : // SH
+                    decoded_instr = INSTR_SH;
+                    break;
+                case 0x2 : // SW
+                    decoded_instr = INSTR_SW;
+                    break;
+            }
+            break;
+        case 0x13 : // Immediate instructions
+            switch (funct3) {
+                case 0x0 : // ADDI
+                    decoded_instr = INSTR_ADDI;
+                    break;
+                case 0x2 : // SLTI
+                    decoded_instr = INSTR_SLTI;
+                    break;
+                case 0x3 : // SLTIU
+                    decoded_instr = INSTR_SLTIU;
+                    break;
+                case 0x4 : // XORI
+                    decoded_instr = INSTR_XORI;
+                    break;
+                case 0x6 : // ORI
+                    decoded_instr = INSTR_ORI;
+                    break;
+                case 0x7 : // ANDI
+                    decoded_instr = INSTR_ANDI;
+                    break;
+                case 0x1 : // SLLI
+                    decoded_instr = INSTR_SLLI;
+                    break;
+                case 0x5 : // SRLI or SRAI
+                    if ((funct7 >> 5) == 0) {
+                        decoded_instr = INSTR_SRLI;
+                    } else {
+                        decoded_instr = INSTR_SRAI;
+                    }
+                    break;
+            }
+            break;
+        case 0x33 : // Register instructions
+            switch (funct3) {
+                case 0x0 : // ADD, SUB
+                    if (funct7 == 0x00) {
+                        decoded_instr = INSTR_ADD;
+                    } else {
+                        decoded_instr = INSTR_SUB;
+                    }
+                    break;
+                case 0x1 : // SLL
+                    decoded_instr = INSTR_SLL;
+                    break;
+                case 0x2 : // SLT
+                    decoded_instr = INSTR_SLT;
+                    break;
+                case 0x3 : // SLTU
+                    decoded_instr = INSTR_SLTU;
+                    break;
+                case 0x4 : // XOR
+                    decoded_instr = INSTR_XOR;
+                    break;
+                case 0x5 : // SRL, SRA
+                    if (funct7 == 0x00) {
+                        decoded_instr = INSTR_SRL;
+                    } else {
+                        decoded_instr = INSTR_SRA;
+                    }
+                    break;
+                case 0x6 : // OR
+                    decoded_instr = INSTR_OR;
+                    break;
+                case 0x7 : // AND
+                    decoded_instr = INSTR_AND;
+                    break;
+            }
+            break;
+        case 0x73 : // ECALL
+            decoded_instr = INSTR_ECALL;
+            break;
+        default :
+            fprintf(stderr, "Error: Unknown instruction\n");
+            exit(1);
+    }
+}
+
+void print_decoded_instr() {
+    switch (decoded_instr) {
+        case INSTR_LUI :
+            printf("LUI\n");
+            break;
+        case INSTR_AUIPC :
+            printf("AUIPC\n");
+            break;
+        case INSTR_JAL :
+            printf("JAL\n");
+            break;
+        case INSTR_JALR :
+            printf("JALR\n");
+            break;
+        case INSTR_BEQ :
+            printf("BEQ\n");
+            break;
+        case INSTR_BNE :
+            printf("BNE\n");
+            break;
+        case INSTR_BLT :
+            printf("BLT\n");
+            break;
+        case INSTR_BGE :
+            printf("BGE\n");
+            break;
+        case INSTR_BLTU :
+            printf("BLTU\n");
+            break;
+        case INSTR_BGEU :
+            printf("BGEU\n");
+            break;
+        case INSTR_LB :
+            printf("LB\n");
+            break;
+        case INSTR_LH :
+            printf("LH\n");
+            break;
+        case INSTR_LW :
+            printf("LW\n");
+            break;
+        case INSTR_LBU :
+            printf("LBU\n");
+            break;
+        case INSTR_LHU :
+            printf("LHU\n");
+            break;
+        case INSTR_SB :
+            printf("SB\n");
+            break;
+        case INSTR_SH :
+            printf("SH\n");
+            break;
+        case INSTR_SW :
+            printf("SW\n");
+            break;
+        case INSTR_ADDI :
+            printf("ADDI\n");
+            break;
+        case INSTR_SLTI :
+            printf("SLTI\n");
+            break;
+        case INSTR_SLTIU :
+            printf("SLTIU\n");
+            break;
+        case INSTR_XORI :
+            printf("XORI\n");
+            break;
+        case INSTR_ORI :
+            printf("ORI\n");
+            break;
+        case INSTR_ANDI :
+            printf("ANDI\n");
+            break;
+        case INSTR_SLLI :
+            printf("SLLI\n");
+            break;
+        case INSTR_SRLI :
+            printf("SRLI\n");
+            break;
+        case INSTR_SRAI :
+            printf("SRAI\n");
+            break;
+        case INSTR_ADD :
+            printf("ADD\n");
+            break;
+        case INSTR_SUB :
+            printf("SUB\n");
+            break;
+        case INSTR_SLL :
+            printf("SLL\n");
+            break;
+        case INSTR_SLT :
+            printf("SLT\n");
+            break;
+        case INSTR_SLTU :
+            printf("SLTU\n");
+            break;
+        case INSTR_XOR :    
+            printf("XOR\n");
+            break;
+        case INSTR_SRL :
+            printf("SRL\n");
+            break;
+        case INSTR_SRA :
+            printf("SRA\n");
+            break;
+        case INSTR_OR :
+            printf("OR\n");
+            break;
+        case INSTR_AND :
+            printf("AND\n");
+            break;
+        case INSTR_ECALL :
+            printf("ECALL\n");
+            break;
+        default :
+            fprintf(stderr, "Error: Unknown instruction\n");
+            exit(1);
+    }
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
@@ -32,6 +349,8 @@ int main(int argc, char **argv) {
             instr |= (uint32_t)buf[i + j] << (j * 8);
         }
         printf("%08x\n", instr);
+        decoder(instr);
+        print_decoded_instr();
     }
 
     return 0;
