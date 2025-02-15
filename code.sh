974ad358d106cec8a73aa46a8f05ce407fe4d068
qtx73 <qtx73nn@gmail.com>
2025-02-15 20:54:49 +0900

    Update GCC flags to support RV32IMV architecture

--- code.sh
+++ code.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-riscv64-unknown-elf-gcc -march=rv32i -mabi=ilp32 -O2 -nostartfiles -T link.ld -o program.elf start.s $1
+riscv64-unknown-elf-gcc -march=rv32imv -mabi=ilp32 -nostartfiles -T link.ld -o program.elf start.s $1
 riscv64-unknown-elf-objcopy -O binary program.elf program.bin
 
