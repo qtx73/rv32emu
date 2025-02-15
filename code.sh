3bacecdd884d7e0c2ff39cfc6f52c6dd79dc6bb9
qtx73 <qtx73nn@gmail.com>
2025-02-15 21:40:20 +0900

    Switch to Clang for RISC-V compilation and update target architecture to RV32IMV

--- code.sh
+++ code.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-riscv64-unknown-elf-gcc -march=rv32imv -mabi=ilp32 -nostartfiles -T link.ld -o program.elf start.s $1
+#riscv64-unknown-elf-gcc -march=rv32imv -mabi=ilp32 -nostartfiles -O2 -T link.ld -o program.elf start.s $1
+
+CC=/usr/local/opt/llvm/bin/clang
+$CC --target=riscv32-unknown-elf -march=rv32imv -mabi=ilp32d -O2 -nostdlib -ffreestanding -T link.ld -o program.elf start.s $1
 riscv64-unknown-elf-objcopy -O binary program.elf program.bin
 
